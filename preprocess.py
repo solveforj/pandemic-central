4f9940474416e5de80a63447b775fde30b475a86
Duy Cao <caominhduy@gmail.com>
2020-06-02 06:33:30 -0500

    Added Johns Hopkins datasets + support
    2020-06-01 data

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,13 @@
 """
-This module preprocesses raw datasets into usable Pandas df
+This module preprocesses raw datasets into usable Pandas df.
+Notice that at early developing stage, we are only focusing on US counties.
 """
 
 import os, sys
 import numpy as np
 import pandas as pd
 from datetime import datetime, date
+from jhu_rename import rename_em
 
 def get_latest_file(src, dir='raw_data/'): # get the directory of the lastest file
     if src == 'apple':
@@ -19,7 +21,18 @@ def get_latest_file(src, dir='raw_data/'): # get the directory of the lastest fi
         max_ = max(new_files)
         lastest_file = 'applemobilitytrends-' + max_[:4] + '-' + max_[4:6] + \
             '-' + max_[6:] + '.csv'
-        lastest_dir = dir + src + '/' + lastest_file
+        path = dir + src + '/' + lastest_file
+
+    if src == 'jhu':
+        files = os.listdir(dir + src + '/county_renamed')
+        new_files = []
+        for file in files:
+            new_file = file.replace('-', '')
+            new_file = new_file.replace('.csv', '')
+            new_files.append(new_file)
+        max_ = max(new_files)
+        lastest_file = max_[:4] + '-' + max_[4:6] + '-' + max_[6:] + '.csv'
+        path = dir + src + '/county_renamed/' + lastest_file
 
     if src == 'kaggle':
         files = os.listdir(dir + src + '/county')
@@ -32,9 +45,9 @@ def get_latest_file(src, dir='raw_data/'): # get the directory of the lastest fi
         max_ = max(new_files)
         lastest_file = 'county-' + max_[:4] + '-' + max_[4:6] + \
             '-' + max_[6:] + '.csv'
-        lastest_dir = dir + src + '/county/' + lastest_file
+        path = dir + src + '/county/' + lastest_file
 
-    return lastest_dir
+    return path
 
 def check_lastest_file(dir): # verify the lastest file with system time
     src = dir.split('/')[1]
@@ -52,15 +65,25 @@ def check_lastest_file(dir): # verify the lastest file with system time
         else:
             return False
 
+    if src == 'jhu':
+        t = date.today().isoformat()
+        if t == dir.replace('.csv', ''):
+            return True
+        else:
+            return False
+
 def get_file_on_date(src, date, dir='raw_data/'): # get the directory of the file on specified date
     if src == 'apple':
-        new_dir = dir + src + '/' + 'applemobilitytrends-' + date + '.csv'
+        path = dir + src + '/' + 'applemobilitytrends-' + date + '.csv'
     if src == 'unacast':
-        new_dir = dir + src + '/county/' + 'COVID_' + date + \
+        path = dir + src + '/county/' + 'COVID_' + date + \
             '_sds-v3-full-county.csv'
-    return new_dir
+    if src == 'jhu':
+        path = dir + src + '/county_renamed/' + date + '.csv'
+    return path
 
 if __name__ == '__main__':
-    test = get_latest_file('kaggle')
+    rename_em()
+    test = get_latest_file('jhu')
     print(test)
     print(check_lastest_file(test))
