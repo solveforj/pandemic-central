0888a08047f4dd1c84275e78c4fa705003f39f10
solveforj <jgalasso@udallas.edu>
2020-06-03 17:26:40 -0700

    Filter and update census data

--- Scripts/preprocess_census.py
+++ Scripts/preprocess_census.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-def census(year):
-    censusData = pd.read_csv("cc-est2018-alldata.csv",  encoding = "ISO-8859-1")
-    censusData = censusData[(censusData['YEAR'] == year - 2007)]
-    censusData.to_csv("census-" + year + ".csv", sep=",", index=False)
-    
+censusData = pd.read_csv("/Users/josephgalasso/Documents/cc-est2018-alldata.csv",  encoding = "ISO-8859-1")
+censusData = censusData[(censusData['YEAR'] == year - 2007)]
+censusData = censusData.loc[:, censusData.columns.isin(['COUNTY','TOT_POP', 'AGEGRP','TOT_MALE'\
+'TOT_FEMALE','H_MALE','H_FEMALE','BA_MALE','BA_FEMALE','IA_MALE','IA_FEMALE',\
+'NA_MALE','NA_FEMALE','TOM_MALE','TOM_FEMALE'])]
+censusData.to_csv("/Users/josephgalasso/Documents/GitHub/pandemic-central/raw_data/census/census-" + str(year) + ".csv", sep=",", index=False)
