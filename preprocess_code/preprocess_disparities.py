3c39690127ac4ac3871b9b9f6d3eac93ba9b67e2
solveforj <jgalasso@udallas.edu>
2020-06-23 11:12:58 -0700

    Clean up

--- preprocess_code/preprocess_disparities.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-disparities = pd.read_csv("raw_data/disparities/CCVI.csv", dtype={"FIPS":str})
-disparities = disparities[disparities.columns[3:-2]].sort_values('FIPS').reset_index(drop=True)
-
-disparities.to_csv("raw_data/disparities/disparities.csv", sep=",", index=False)
