2d5461290549f1c3e95eba58f40ffb65cf445949
solveforj <jgalasso@udallas.edu>
2020-08-08 20:59:21 -0700

    Update predict.py

---
+++ model/predict.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import pickle
+import os
+from datetime import date
+from sklearn.preprocessing import StandardScaler
+
+__author__ = 'Duy Cao, Joseph Galasso'
+__copyright__ = '© Pandamic Central, 2020'
+__license__ = 'MIT'
+__version__ = '2.0.0'
+__status__ = 'released'
+__url__ = 'https://github.com/solveforj/pandemic-central'
+
+def make_predictions():
+    date_today = date.today().strftime('%Y-%m-%d')
+
+    mobility_data = pd.read_csv(os.path.split(os.getcwd())[0] + "/" + "mobility_full_predictions.csv.gz", dtype={"label":float})
+    no_mobility_data = pd.read_csv(os.path.split(os.getcwd())[0] + "/" + "no_mobility_full_predictions.csv.gz",dtype={"label":float})
+
+    latest_dates = list(range(((9*-7)-1), 6, 7))
+    latest_mobility = mobility_data.groupby("FIPS", as_index=False).nth(latest_dates)
+    latest_no_mobility = no_mobility_data.groupby("FIPS", as_index=False).nth(latest_dates)
+
+
+    combined_predictions = latest_mobility.append(latest_no_mobility, ignore_index=True)
+    combined_predictions = combined_predictions.sort_values(['FIPS', 'date', 'fb_movement_change'], na_position='first').groupby(['FIPS', 'date']).tail(1)
+    combined_predictions = combined_predictions.groupby("FIPS").tail(10)
+
+
+    combined_predictions['fb_movement_change'] = combined_predictions['fb_movement_change'].astype(float)
+    combined_predictions['fb_stationary'] = combined_predictions['fb_stationary'].astype(float)
+    combined_predictions.iloc[:, 5:] = combined_predictions.iloc[:, 5:].round(3)
+
+    id = combined_predictions['Location'] + ", " + combined_predictions['FIPS'].astype(str) + ", " + combined_predictions['region']
+    combined_predictions.insert(0, 'ID', id)
+    combined_predictions = combined_predictions.fillna("NULL")
+    combined_predictions = combined_predictions.astype(str)
+
+    combined_predictions.to_csv("predictions/projections/predictions_" + date_today + ".csv", index=False)
+    combined_predictions.to_csv("predictions/projections/predictions_latest.csv", index=False)
+
+if __name__ == "__main__":
+    make_predictions()
