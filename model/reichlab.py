5ab4dbe562d432f684dfa96d308f70e9a7a01789
Duy Cao <caominhduy@gmail.com>
2020-12-04 03:14:39 -0600

    Modified reichlab.py for support of quantiles

--- model/reichlab.py
+++ model/reichlab.py
@@ -1,103 +1,106 @@
 """
 This module processes output data into right format.
-Processed data can be found at
+Processed data can be found @ cdc.gov & Reichlab's GitHub repository
 """
 
 import numpy as np
 import pandas as pd
 from datetime import datetime, date, timedelta
 import os
-import yaml
 from isoweek import Week
 
-PREDICTION_FILE = 'predictions/web/predictions_latest.csv'
-PREDICTION_COLUMNS = ['FIPS', 'date', 'model_predictions']
+__author__ = 'Duy Cao, Joseph Galasso'
+__copyright__ = '© Pandamic Central, 2020'
+__license__ = 'MIT'
+__status__ = 'release'
+__url__ = 'https://github.com/solveforj/pandemic-central'
+__version__ = '2.1.0'
 
-# Require isoweek
+PREDICTION_FILE = 'predictions/projections/predictions_latest.csv'
+PREDICTION_COLUMNS = ['FIPS', 'date']
+WEEKS = ['1', '2', '3', '4']
+QUANTILES = ['0.025', '0.1', '0.25', '0.5', '0.75', '0.9', '0.975']
+
+# Require package 'isoweek'
+# try command 'pip install isoweek'
+
 def get_saturday(d):
-    #
-    # EPIWEEK ENDS ON SATURDAY
+    # EPIWEEK ENDS ON SATURDAY...
     # If the forecast date is Sunday or Monday, the first week will end on the
     # next Saturday. Otherwise, first week should end on the Saturday of the second
     # week.
-    #
     date = d.isocalendar()
-    if date[2] == 1:
+    if date[2] <= 1:
         final = str(date[0]) + 'W' + str((date[1]))
         next_sat = Week.fromstring(final).saturday()
     else:
         final = str(date[0]) + 'W' + str((date[1] + 1))
         next_sat = Week.fromstring(final).saturday()
-    return next_sat
+    return next_sat.isoformat()
 
 def read_prediction():
-    # Read CSV predictions and get the last date data
-    #
-    # In which:
-    #   - 'model_predictions': prediction data for the week after next week
-    #   - 'nextweek_predictions': predictions for the next closest week
+    # Complete the list of important column headers
+    for week in WEEKS:
+        for quantile in QUANTILES:
+            PREDICTION_COLUMNS.append(f'quantile_{quantile}_{week}_weeks')
+        PREDICTION_COLUMNS.append(f'point_{week}_weeks')
 
+    # Read projections
     df_predict = pd.read_csv(PREDICTION_FILE, usecols=PREDICTION_COLUMNS)
-    df_predict = df_predict.groupby('FIPS').tail(2).reset_index(drop=True)
-    df_predict = df_predict.rename(columns={'model_predictions': 'value'})
+    df_predict = df_predict.groupby('FIPS').tail(1).reset_index(drop=True)
+
+    # Set date of forecast
+    df_predict = df_predict.drop(['date'], 1)
 
     # Write FIPS in 5-digit string format
     df_predict['FIPS'] = df_predict['FIPS'].astype('str')
     df_predict['FIPS'] = df_predict['FIPS'].str.zfill(5)
-    df_predict = df_predict.reset_index(drop=True)
+    df_predict = df_predict.rename(columns={'FIPS': 'location'})
 
-    # Rename some columns
-    df_predict = df_predict.rename(columns={'FIPS': 'location', 'date': 'forecast_date'})
+    # List of values to be exploded
+    df_predict['value'] = df_predict[PREDICTION_COLUMNS[2:]].values.tolist()
 
-    # 1st week from forecast date
-    df_predict_1 = df_predict.groupby('location').head(1).reset_index(drop=True)
-    df_predict_1['value'] = df_predict_1['value'] * 7
-    # Calculate end date of the first week
-    df_predict_1['target_end_date'] = pd.to_datetime(df_predict_1['forecast_date'])
-    df_predict_1['target_end_date'] = get_saturday(pd.to_datetime(df_predict_1['forecast_date'])[0])
-    df_predict_1['target'] = '1 wk ahead inc case'
-    # Remove some counties with day lag
-    latest = df_predict_1['forecast_date'].unique()[0]
-    df_predict_1 = df_predict_1[df_predict_1['forecast_date'] == latest]
+    # List of targets to be exploded
+    targets = []
+    for wk in WEEKS:
+        targets = targets + [f'{wk} wk ahead inc case']*8
+    df_predict['target'] = [targets] * len(df_predict)
 
-    # 2nd week from forecast date
-    df_predict_2 = df_predict.groupby('location').tail(1).reset_index(drop=True)
-    df_predict_2['value'] = df_predict_2['value'] * 7
-    # Calculate end date of the second week
-    df_predict_2['target_end_date'] = pd.to_datetime(df_predict_2['forecast_date'])
-    df_predict_2['target_end_date'] = get_saturday(pd.to_datetime(df_predict_2['forecast_date'])[0])
-    df_predict_2['target'] = '2 wk ahead inc case'
-    # Remove some counties with day lag
-    latest = df_predict_2['forecast_date'].unique()[0]
-    df_predict_2 = df_predict_2[df_predict_2['forecast_date'] == latest]
+    # List of types to be exploded
+    types = ['quantile']*7+['point']
+    types = types * len(WEEKS)
+    df_predict['type'] = [types] * len(df_predict)
 
-    # Merge two datasets together
-    df_predict = pd.concat([df_predict_1, df_predict_2], ignore_index=True, sort=False)
-    df_predict = df_predict.sort_values(by=['location', 'target']).reset_index(drop=True)
+    # List of quantiles to be exploded
+    df_predict['quantile'] = [(QUANTILES + ['NA'])*len(WEEKS)] * len(df_predict)
 
-    # Fix negative value bug and round number
-    df_predict['value'] = df_predict['value'].mask(df_predict['value'] < 0, 0)
-    df_predict['value'] = df_predict['value'].round().astype(int)
+    # List of end dates to be exploded
+    ends = []
+    for wk in range(0,4):
+        ends = ends + [get_saturday(date.today() + timedelta(weeks=wk))]*8
+    df_predict['target_end_date'] = [ends] * len(df_predict)
 
-    # Add required columns for convention
-    df_predict['type'] = 'point'
-    df_predict['quantile'] = 'NA'
+    # Drop all unnecessary columns
+    df_predict = df_predict.drop(PREDICTION_COLUMNS[2:], 1)
 
-    latest = df_predict['forecast_date'].unique()[0]
-    df_predict['forecast_date'] = latest
+    # EXPLODE
+    df_predict = df_predict.set_index(['location']).apply(pd.Series.explode).reset_index()
 
-    # Export data
-    filename = latest
+    # Set 'forecast_date' to be the date today
+    df_predict['forecast_date'] = date.today().isoformat()
 
+    # Reorder columns
+    df_predict = df_predict[['location', 'forecast_date', 'value', \
+                            'target_end_date', 'target', 'type', 'quantile']]
+    # Preview
+    print(df_predict.head(20))
+
+    # EXPORT DATA
+    filename = date.today().isoformat()
     if not os.path.exists('predictions/covid19-forecast-hub'):
         os.mkdir('predictions/covid19-forecast-hub')
-
     filepath = 'predictions/covid19-forecast-hub/' + filename + '-PandemicCentral-USCounty.csv'
     df_predict.to_csv(filepath, index=False)
-
-    print('Unique forecast date:', df_predict['forecast_date'].unique(), '\n')
-    print('Unique end date:', df_predict['target_end_date'].unique(), '\n')
-    print(df_predict.head(10))
 
 def metadata():
     meta = {
