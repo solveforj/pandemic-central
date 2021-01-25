8dfdb13875959322e578ade751a924a0f8507e32
Duy Cao <caominhduy@gmail.com>
2021-01-25 01:48:53 -0600

    Updated code for state-level data processing + vaccine maps + latest data

--- data/JHU/preprocess.py
+++ data/JHU/preprocess.py
@@ -19,11 +19,35 @@ __status__ = 'release'
 __url__ = 'https://github.com/solveforj/pandemic-central'
 __version__ = '2.0.0'
 
-def preprocess_JHU():
-    print('• Processing JHU Case Data')
+def get_rt_live_state_lv():
+    print('• Processing JHU Data - State Level')
+    df = pd.read_csv('data/JHU/jhu_data.csv',\
+                    usecols=['FIPS', 'date', 'confirmed_cases', 'confirmed_cases_norm'],\
+                    dtype={'FIPS':'str'},
+                    low_memory=False)
+    df_death = pd.read_csv('data/JHU/jhu_data_deaths.csv',\
+                    usecols=['FIPS', 'date', 'confirmed_deaths', 'confirmed_deaths_norm'],\
+                    dtype={'FIPS':'str'},
+                    low_memory=False)
+    df = df.merge(df_death, on=['FIPS', 'date'])
+    geo = pd.read_csv('data/geodata/state_fips.csv',\
+                    usecols=[1,2])
+    geo_dict = dict(zip(geo.fips, geo.abbr))
+    df['FIPS'] = df['FIPS'].str[:2].astype('int').replace(geo_dict)
+    df = df.groupby(['FIPS', 'date']).sum().reset_index()
+    df = df.rename(columns={'FIPS':'state'})
+    df.to_csv('data/JHU/state_jhu_data.csv', index=False)
+    print('  Finished\n')
+
+def preprocess_JHU(type='cases'):
+    print(f'• Processing JHU {type.capitalize()} Data - County Level')
 
     # Get all other data from Johns Hopkins
-    jhu_data = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv").dropna()
+    if type == 'cases':
+        jhu_data = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv").dropna()
+    elif type == 'deaths':
+        jhu_data = pd.read_csv(f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{type}_US.csv").dropna()
+
     jhu_data['FIPS'] = jhu_data['FIPS'].astype(int).astype(str)
 
     jhu_data = jhu_data[jhu_data['FIPS'].notnull()]
@@ -34,20 +58,24 @@ def preprocess_JHU():
 
     jhu_data['FIPS'] = jhu_data['FIPS'].apply(lambda x : process_FIPS(x))
 
-    jhu_data = jhu_data.drop(["Admin2","Province_State","Country_Region","Lat","Long_","Combined_Key","UID","iso2","iso3","code3"], axis=1)
-    jhu_data = jhu_data.melt(id_vars=['FIPS'], var_name = 'date', value_name = 'confirmed_cases')
+    if type == 'cases':
+        jhu_data = jhu_data.drop(["Admin2","Province_State","Country_Region","Lat","Long_","Combined_Key","UID","iso2","iso3","code3"], axis=1)
+    elif type == 'deaths':
+        jhu_data = jhu_data.drop(["Admin2","Province_State","Country_Region","Lat","Long_","Combined_Key","UID","iso2","iso3","code3", "Population"], axis=1)
+
+    jhu_data = jhu_data.melt(id_vars=['FIPS'], var_name = 'date', value_name = f'confirmed_{type}')
     jhu_data['date'] = pd.to_datetime(jhu_data['date'])
     jhu_data = jhu_data.sort_values(['FIPS', 'date'])
 
     # Case counts are cumulative and will be converted into daily change
-    jhu_data['confirmed_cases'] = jhu_data.groupby('FIPS')['confirmed_cases'].diff().dropna()
+    jhu_data[f'confirmed_{type}'] = jhu_data.groupby('FIPS')[f'confirmed_{type}'].diff().dropna()
     full_data = jhu_data
     full_data = full_data.sort_values(['FIPS','date'], axis=0)
     full_data = full_data.reset_index(drop=True)
 
     # Compute 14-day (weekly) rolling average of cases for each county
-    full_data['confirmed_cases'] = pd.Series(full_data.groupby("FIPS")['confirmed_cases'].rolling(7).mean()).reset_index(drop=True)
-    full_data = full_data[full_data['confirmed_cases'].notnull()]
+    full_data[f'confirmed_{type}'] = pd.Series(full_data.groupby("FIPS")[f'confirmed_{type}'].rolling(7).mean()).reset_index(drop=True)
+    full_data = full_data[full_data[f'confirmed_{type}'].notnull()]
 
     # Move dates forward by 1 day so that movement averages represent data from past week
     full_data['date'] = pd.to_datetime(full_data['date'])
@@ -61,15 +89,23 @@ def preprocess_JHU():
 
     merged_df = pd.merge(left=census_data, right=full_data, how='left', on='FIPS', copy=False)
 
-    merged_df['confirmed_cases_norm'] = (merged_df['confirmed_cases']/merged_df['TOT_POP'] * 100000).round()
+    merged_df[f'confirmed_{type}_norm'] = (merged_df[f'confirmed_{type}']/merged_df['TOT_POP'] * 100000).round()
     merged_df = merged_df.drop('TOT_POP', axis=1)
     startdate = datetime.strptime('2020-2-15', '%Y-%m-%d')
     merged_df['date'] = pd.to_datetime(merged_df['date'])
     merged_df = merged_df[merged_df['date'] >= startdate]
 
-    merged_df.to_csv('data/JHU/jhu_data.csv', sep=",", index=False)
+    if type == 'cases':
+        merged_df.to_csv('data/JHU/jhu_data.csv', sep=",", index=False)
+    elif type == 'deaths':
+        merged_df.to_csv(f'data/JHU/jhu_data_{type}.csv', sep=",", index=False)
 
     print('  Finished\n')
+
+    if type == 'cases':
+        preprocess_JHU(type='deaths')
+    elif type == 'deaths':
+        get_rt_live_state_lv()
 
 if __name__ == "__main__":
     preprocess_JHU()
--- data/google/preprocess.py
+++ data/google/preprocess.py
@@ -65,7 +65,7 @@ def google_mobility_to_pd(df_load):
     df_google = df_google.reset_index(drop=True)
 
     # Compute 14 day rolling averages for movement data
-    df_google['google_mobility'] = pd.Series(df_google.groupby('fips')['google_mobility'].rolling(14).mean()).reset_index(drop=True)
+    df_google['google_mobility'] = pd.Series(df_google.groupby('fips')['google_mobility'].rolling(7).mean()).reset_index(drop=True)
 
     # Move dates forward by 1 day so that movement averages represent data from past week
     df_google['date'] = pd.to_datetime(df_google['date'])
@@ -92,11 +92,13 @@ def google_mobility_state_level():
     df = df.drop(['country_region_code', 'country_region', 'sub_region_1',\
                 'sub_region_2', 'metro_area', 'census_fips_code'], 1)
     df = df.sort_values(by=['state', 'date']).reset_index(drop=True)
+    for col in df.columns[2:]:
+        df[col] = df.groupby('state')[col].rolling(7).mean().reset_index(drop=True)
     df.to_csv('data/google/state_mobility.csv', index=False)
     print('  Finished\n')
 
 def preprocess_google():
-    print('• Processing Google Mobility Data')
+    print('• Processing Google Mobility Data - County Level')
     status = google_url_health()
     if status:
         df = get_google_data()
@@ -104,6 +106,7 @@ def preprocess_google():
         print('  Finished\n')
     else:
         print('  Error - Google Mobility Data could not be found from server\n')
+    google_mobility_state_level()
 
 if __name__ == "__main__":
     preprocess_google()
